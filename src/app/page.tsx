4a10bd1e06e14baaef555a0cc0d40e112b384cd6
joaoncfsantos <santos.joaonuno2000@gmail.com>
2023-07-09 11:00:45 +0100

    random

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,106 +66,100 @@ export default function Home() {
           {/* <Link href="/questspage">
             <QuestsButton />
           </Link> */}
-          <div onClick={handleShowQuests}>
-            <QuestsButton />
-          </div>
           <a href="#donations">
             <Donate />
+          </a>
+          <a href="#quests">
+            {/* <div onClick={handleShowQuests}> */}
+            <QuestsButton />
+            {/* </div> */}
           </a>
           <ConnectWallet />
         </div>
       </div>
       {/* -------------------------------------------------------------------------------------------- */}
-      {showQuests && (
-        <div>
-          <div className="flex justify-center">
-            <div className="flex flex-col text-center justify-center gap-6 mt-36">
-              <h2 className="text-6xl">Available Quests</h2>
-              <h3 className="text-xl font-medium">
-                Here you can find all the quests
-              </h3>
-            </div>
-          </div>
-          <div className="flex flex-col p-12 gap-8 justify-center items-center">
-            <div className="flex flex-col gap-12">
-              <QuestCard
-                title="Barcelona Quest 1"
-                creator="@BernardoReiis"
-                description="This is a quest about Barcelona"
-                group="Hackathon BCN"
-                isCompleted={true}
-              />
-              <QuestCard
-                title="Barcelona Quest 2"
-                creator="@GugaRodri"
-                description="This is another quest about Barcelona"
-                group="Hackathon BCN"
-                isCompleted={false}
-              />
-              <QuestCard
-                title="Attend ETHBarcelona"
-                creator="@BernardoReiis"
-                description="Build an amazing telegram bot with Web2 and Web3"
-                group="Hackathon BCN"
-                isCompleted={false}
-              />
-            </div>
-          </div>
-        </div>
-      )}
 
-      {showMain && (
-        <div>
-          <div className="flex flex-col justify-center">
-            <div className="flex flex-col text-center justify-center gap-6 mt-36">
-              <h2 className="text-4xl">
-                Communifying your donations and impact!
-              </h2>
-              <h3 className="text-lg font-medium">
-                Why donate alone when you can do it with your community?
-              </h3>
-            </div>
-            <div className="flex items-center flex-col text-center justify-center gap-4 mt-20">
-              <p className="text-lg">Powered by:</p>
-              <div className="flex justify-center items-center gap-4">
-                <Image
-                  src={"/endaoment_logo.png"}
-                  height={100}
-                  width={100}
-                  alt=""
-                />
-                <Image
-                  src={"/scroll_logo.jpg"}
-                  height={100}
-                  width={150}
-                  alt=""
-                />
-                <Image src={"/zksync.png"} height={100} width={140} alt="" />
-              </div>
+      <div>
+        <div className="flex flex-col justify-center">
+          <div className="flex flex-col text-center justify-center gap-6 mt-36">
+            <h2 className="text-4xl">
+              Communifying your donations and impact!
+            </h2>
+            <h3 className="text-lg font-medium">
+              Why donate alone when you can do it with your community?
+            </h3>
+          </div>
+          <div className="flex items-center flex-col text-center justify-center gap-4 mt-20">
+            <p className="text-lg">Powered by:</p>
+            <div className="flex justify-center items-center gap-4">
+              <Image
+                src={"/endaoment_logo.png"}
+                height={100}
+                width={100}
+                alt=""
+              />
+              <Image src={"/scroll_logo.jpg"} height={100} width={150} alt="" />
+              <Image src={"/zksync.png"} height={100} width={140} alt="" />
             </div>
           </div>
-          <div
-            id="donations"
-            className="flex flex-col p-12 gap-12 justify-center items-center"
-          >
-            <OrganizationCard
-              name={orgs[2]?.name}
-              description={orgs[2]?.description}
-              contractAddress={orgs[2]?.contractAddress}
+        </div>
+        <div
+          id="donations"
+          className="flex flex-col p-12 gap-12 justify-center items-center"
+        >
+          <OrganizationCard
+            name={orgs[2]?.name}
+            description={orgs[2]?.description}
+            contractAddress={orgs[2]?.contractAddress}
+          />
+          <OrganizationCard
+            name={orgs[13]?.name}
+            description={orgs[13]?.description}
+            contractAddress={orgs[13]?.contractAddress}
+          />
+          <OrganizationCard
+            name={orgs[14]?.name}
+            description={orgs[14]?.description}
+            contractAddress={orgs[14]?.contractAddress}
+          />
+        </div>
+      </div>
+
+      <div id="quests">
+        <div className="flex justify-center">
+          <div className="flex flex-col text-center justify-center gap-6 mt-36">
+            <h2 className="text-6xl">Available Quests</h2>
+            <h3 className="text-xl font-medium">
+              Here you can find all the quests
+            </h3>
+          </div>
+        </div>
+        <div className="flex flex-col p-12 gap-8 justify-center items-center">
+          <div className="flex flex-col gap-12">
+            <QuestCard
+              title="Barcelona Quest 1"
+              creator="@BernardoReiis"
+              description="This is a quest about Barcelona"
+              group="Hackathon BCN"
+              isCompleted={true}
             />
-            <OrganizationCard
-              name={orgs[13]?.name}
-              description={orgs[13]?.description}
-              contractAddress={orgs[13]?.contractAddress}
+            <QuestCard
+              title="Barcelona Quest 2"
+              creator="@GugaRodri"
+              description="This is another quest about Barcelona"
+              group="Hackathon BCN"
+              isCompleted={false}
             />
-            <OrganizationCard
-              name={orgs[14]?.name}
-              description={orgs[14]?.description}
-              contractAddress={orgs[14]?.contractAddress}
+            <QuestCard
+              title="Attend ETHBarcelona"
+              creator="@BernardoReiis"
+              description="Build an amazing telegram bot with Web2 and Web3"
+              group="Hackathon BCN"
+              isCompleted={false}
             />
           </div>
         </div>
-      )}
+      </div>
     </div>
   );
 }
